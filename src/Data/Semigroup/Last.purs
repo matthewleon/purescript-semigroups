0a7c6616c70c64c1d9935beb3945395a5dfa15e9
Nicholas Scheel <monoidmusician@gmail.com>
2018-03-05 10:37:04 -0600

    Add Functor and Alt instances for First, Last

--- src/Data/Semigroup/Last.purs
+++ src/Data/Semigroup/Last.purs
@@ -1,9 +1,10 @@
 module Data.Semigroup.Last where
-  
+
 import Prelude
 
+import Control.Alt (class Alt)
 import Data.Newtype (class Newtype)
-  
+
 newtype Last a = Last a
 
 derive instance newtypeLast :: Newtype (Last a) _
@@ -16,3 +17,9 @@ instance showLast :: Show a => Show (Last a) where
 
 instance semigroupLast :: Semigroup (Last a) where
   append _ x = x
+
+instance functorLast :: Functor Last where
+  map f (Last x) = Last (f x)
+
+instance altLast :: Alt Last where
+  alt _ x = x
